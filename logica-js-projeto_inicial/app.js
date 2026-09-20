ac314796fafce52bf5de84b62aa1021d7aaca95e
Rodrigo de Sousa Mota <rodrigo.mota@solutis.com.br>
2026-09-20 15:21:03 -0300

    projeto inicial

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,36 @@
+alert('Boas Vindas ao jogo do número Secreto');
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+
+console.log(numeroSecreto);
+
+let chute;
+let tentativas = 1;
+
+
+// Enquanto o chute não for igual ao n.s.
+while (chute != numeroSecreto ){
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    // Se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`o número secreto é menor que ${chute}`);
+    }     else {
+            alert(`o número secreto é maior ${chute}`);
+    }
+    
+    // tentativas = tentativas +1;
+    tentativas++;
+    }
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'Tentativa';
+
+
+//if (tentativas > 1) { 
+//    alert(`Isso ai! você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+//} else {
+//    alert(`Isso ai! você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+//}    
